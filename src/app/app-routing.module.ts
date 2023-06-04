4ca54521388808d42b8409892fe416cf4de36b61
AleixPrueba <aleix.prat4@gmail.com>
2023-06-04 11:12:27 +0200

    aleix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './components/formulario/login-form/login-form.component';
+import { AlmacenesComponent } from './components/tabla/almacenes/almacenes.component';
+import { CamionesComponent } from './components/tabla/camiones/camiones.component';
+import { MaterialesComponent } from './components/tabla/materiales/materiales.component';
+import { PedidosComponent } from './components/tabla/pedidos/pedidos.component';
+import { DetallePedidoFormComponent } from './components/formulario/detalle-pedido-form/detalle-pedido-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: '/login'},
+  { path: 'login', component: LoginFormComponent},
+  { path: 'almacenes', component: AlmacenesComponent},
+  { path: 'camiones', component: CamionesComponent},
+  { path: 'materiales', component: MaterialesComponent},
+  { path: 'pedidos', component: PedidosComponent},
+  { path: 'almacenes', component: AlmacenesComponent},
+  { path: 'pedido/nuevo', component: DetallePedidoFormComponent},
+  { path: 'pedido/:idPedido', component: DetallePedidoFormComponent},
+  { path: 'pedido/:idPedido', component: DetallePedidoFormComponent},
+
+  { path: '**', redirectTo: '/login' }
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
