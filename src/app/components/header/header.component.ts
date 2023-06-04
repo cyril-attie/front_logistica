52f07cbdf32c0b8c325599b7dbbfbdd1b641579a
AleixPrueba <aleix.prat4@gmail.com>
2023-06-04 18:00:24 +0200

    header ap4

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { UsuariosServiceService } from 'src/app/servicios/usuarios-service.service';
 
 @Component({
@@ -10,15 +11,22 @@ export class HeaderComponent {
 
   isLogged : boolean;
 
-  constructor(private usuariosServices: UsuariosServiceService) {
+  constructor(private usuariosServices: UsuariosServiceService,
+              private router: Router) {
     this.isLogged = false;
   }
 
   ngOnInit() {
-    //Sabemos si esta logeado o no en esta propiedad - APSP
+    //Sabemos si esta logeado o no, con esta propiedad "isLogged" - APSP
     this.usuariosServices.isLogged.subscribe(value => {
       this.isLogged = value;
     });
+  }
+
+  logout() {
+    localStorage.removeItem('token_almacen');
+    this.usuariosServices.changeLogin(false);
+    this.router.navigate(['/login']);
   }
 
 }
