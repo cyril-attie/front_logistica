b3d69626ed5bc0970bc8a708f72fd5ad4f28a382
Sara G.A <s.garrofe@gmail.com>
2023-06-05 15:18:22 +0200

    detalle-usuario-form

--- src/app/components/formulario/detalle-usuario-form/detalle-usuario-form.component.ts
+++ src/app/components/formulario/detalle-usuario-form/detalle-usuario-form.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-detalle-usuario-form',
   templateUrl: './detalle-usuario-form.component.html',
   styleUrls: ['./detalle-usuario-form.component.sass']
 })
-export class DetalleUsuarioFormComponent {
+export class DetalleUsuarioFormComponent implements OnInit{
 
+  usuarioForm: FormGroup;
+
+  constructor(){
+    this.usuarioForm = new FormGroup({
+      nombre: new FormControl('', [Validators.required]),
+      apellido: new FormControl('', [Validators.required]),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)]),
+      contrasena: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      pais: new FormControl('', [Validators.required]),
+      ciudad: new FormControl('', [Validators.required]),
+      codigo_postal: new FormControl('', [
+        Validators.required, 
+        Validators.pattern('^(0[1-9]|[1-4]\\d|5[0-2])\\d{3}$')]),
+      edad: new FormControl('', [Validators.required]),
+      rol: new FormControl('', [Validators.required]),
+      estado: new FormControl('', [Validators.required]),
+    }, []);
+  }
+
+  ngOnInit(){
+  }
+
+
+  datosUsuarios(){
+    this.usuarioForm.value
+
+  }
 }
