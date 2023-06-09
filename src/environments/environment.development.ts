2875767f91a8749b171e921e117840c983aec3f1
Sara G.A <s.garrofe@gmail.com>
2023-06-09 17:09:49 +0200

    nueva api y no guarda id en el localstorage

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://34.65.131.41:3000'
+    apiUrl: 'http://34.65.146.44:3000'
 };
 
 //apiUrl: 'http://localhost:3000'
-//apiUrl: 'http://34.65.131.41:3000'
+//apiUrl: 'http://34.65.146.44:3000'
