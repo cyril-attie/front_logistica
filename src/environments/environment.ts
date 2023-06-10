8e94f23b92c6470ffc287bb0039e44c0f669124e
AleixPrueba <aleix.prat4@gmail.com>
2023-06-10 13:35:39 +0200

    environment actualizado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:3000'
+    apiUrl: 'http://34.65.194.208:3389'
 };
 
 //apiUrl: 'http://localhost:3000'
-//apiUrl: 'http://34.65.194.208:3000'
+//apiUrl: 'http://34.65.194.208:3389'
