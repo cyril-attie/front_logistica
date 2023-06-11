6846997cd50b9cec6836708149ccdfb46e545dea
Sara G.A <s.garrofe@gmail.com>
2023-06-11 09:32:16 +0200

    materiales services e inicio componente

--- src/app/components/tabla/materiales/materiales.component.ts
+++ src/app/components/tabla/materiales/materiales.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { PropiedadesTabla } from 'src/app/interfaces/propiedades-tabla';
+import { MaterialService } from 'src/app/servicios/material.service';
+import { TablaRefreshService } from 'src/app/servicios/tabla-refresh.service';
 
 @Component({
   selector: 'app-materiales',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./materiales.component.sass']
 })
 export class MaterialesComponent {
+
+
+
+  propiedadesTabla: PropiedadesTabla = {
+    response: [],
+    columnas: [],
+    claves: [],
+    botones: {
+      ver : false,
+      editar : false,
+      borrar: false,
+    },
+    url_param: ""
+  };
+
+  
+  constructor(
+    private materialesService: MaterialService, 
+    private tablaRefreshService: TablaRefreshService){
+
+
+  }
+
+
 
 }
