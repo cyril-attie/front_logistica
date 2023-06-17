5f5bb2e0114c5b0afd14f823a8650d8772145ab0
Sara G.A <s.garrofe@gmail.com>
2023-06-17 21:19:26 +0200

    tabla almacen stock inacabada

---
+++ src/app/interfaces/stock-material.ts
@@ -0,0 +1,8 @@
+export interface StockMaterial {
+    posicion: number;
+    stocks_id: number;
+    descripcion_material?: string;
+    descripcion_categoria?: string;
+    total_unidades: number;
+    materiales_id: number
+}
